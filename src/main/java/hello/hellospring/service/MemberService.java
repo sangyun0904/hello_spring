45219acd7212d8f3aa42f84b94cf9acc2389b570
sangyoon kim <sangyun0904@gmail.com>
2022-12-29 19:05:09 +0900

    Modify join to public, MemberService memberRepository DI create memberServiceTest 회원 서비스 테스트

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,19 +2,23 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     /*
     *  회원 가입
     */
-    private long join(Member member) {
+    public long join(Member member) {
         // 같은 이름 중복 회원 X
         validateDuplicateMember(member);
         memberRepository.save(member);
