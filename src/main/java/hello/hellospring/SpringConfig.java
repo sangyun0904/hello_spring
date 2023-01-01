76ed3a5613ecaac228862d6d6a3a5b2fb70580c4
sangyoon kim <sangyun0904@gmail.com>
2023-01-01 12:09:26 +0900

    Create MSItest and JdbcMemberRepository.java h2 db연결, 통합 테스트

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,26 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 정형화 되지 않거나 상황에 따라 구현 클래스 변경해야 하면 설정을 통해 스프링 빈으로 등록!!!
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -18,7 +29,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        return new JdbcMemberRepository(dataSource);
+        // return new MemoryMemberRepository();
     }
 }
 
