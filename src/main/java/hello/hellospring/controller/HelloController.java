97e057a20e834ed776b98fb4dbf943b4c25686cf
Sangyoon Kim <69445075+sangyun0904@users.noreply.github.com>
2023-01-04 20:34:39 +0900

    Update HelloController.java

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("hello") // uri path
     public String hello(Model model) {
         model.addAttribute("data", "spring!!");
         return "hello"; // return hello -> templates/hello.html
