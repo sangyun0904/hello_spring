b16a7910c7a70a58d50bfdb13583a9ce03031fb1
sangyoon kim <sangyun0904@gmail.com>
2023-01-22 15:38:58 +0900

    Add select one, create, delete employee methods in employee controller

--- src/main/java/hello/hellospring/controller/EmployeeController.java
+++ src/main/java/hello/hellospring/controller/EmployeeController.java
@@ -2,8 +2,7 @@ package hello.hellospring.controller;
 
 import hello.hellospring.domain.Employee;
 import hello.hellospring.repository.EmployeeRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +18,40 @@ public class EmployeeController {
     @GetMapping("/employees")
     List<Employee> all() {
         return repository.findAll();
+    }
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+
+    // Single item
+//
+//    @GetMapping("/employees/{id}")
+//    Employee one(@PathVariable Long id) {
+//
+//        return repository.findById(id)
+//                .orElseThrow(() -> new EmployeeNotFoundException(id));
+//    }
+
+    @PutMapping("/employees/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newEmployee.setId(id);
+                    return repository.save(newEmployee);
+                });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 
 }
